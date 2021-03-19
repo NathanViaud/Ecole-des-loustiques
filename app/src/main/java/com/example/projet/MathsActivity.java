babfb16c95317de55556db0fe96557160e1ebc98
delhomba <baptiste.delhomme1@etu.univ-grenoble-alpes.fr>
2021-03-19 18:13:26 +0100

    testccommit

--- app/src/main/java/com/example/projet/MathsActivity.java
+++ app/src/main/java/com/example/projet/MathsActivity.java
@@ -3,6 +3,7 @@ package com.example.projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MathsActivity extends AppCompatActivity {
 
